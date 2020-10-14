3b1beb4b7e5f6b10d412c6410df3e38c3b740f24
QA-Alam <alam.itprofessinal@gmailcom>
2020-10-14 17:21:32 -0400

    Commit new class for smoething test

---
+++ SmartTech_Automation/src/test/java/slenium/automation/NewTest.java
@@ -0,0 +1,10 @@
+package slenium.automation;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This Git-Hub Test");
+	}
+
+}
